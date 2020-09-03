b654131149ea0820c0d7c28c533ee533458a1216
Jacob Levitin <jlevitin92@gmail.com>
2020-09-03 01:39:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # Write your #display_rainbow method here
-def display_rainbow
+def display_rainbow(colors)
 
 letters = ["R", "O", "Y", "G", "B", "I", "V"]
+
+puts "#{letters[0]}: colors[0], "
 
 end
